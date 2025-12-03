24e284d6f1aab764642150cd0d391018cf348ace
Carlos Eduardo <carloseduardodudu558@hotmail.com>
2025-12-02 21:09:32 -0300

    Update noticia.js

--- js/noticia.js
+++ js/noticia.js
@@ -12,7 +12,7 @@ document.querySelector('#LerMais').href = urlNoticia;
 
 // fetch(`https://newsapi.org/v2/everything?q=brasil&from=${diaAnterior}&sortBy=publishedAt&apiKey=366a4893cc3440b486f45b01e7fe90a5`).then(response => response.json()).then(data => {
 
-fetch("./../DataBase.json").then(response => response.json()).then(data => {
+fetch("./DataBase.json").then(response => response.json()).then(data => {
     const noticiaUrl = urlNoticia;
     const artigo = data.articles.find((article) => article.url === noticiaUrl);
     document.querySelector("#titulo").innerHTML=artigo.title;
